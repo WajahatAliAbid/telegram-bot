a1479a4679bea67f7de0ea3177b340ce240f0a4e
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2021-02-14 05:16:20 +0500

    added logging

--- src/MyExampleBot/Commands/HelloCommand.cs
+++ src/MyExampleBot/Commands/HelloCommand.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Telegram.Bot.Framework;
 using Telegram.Bot.Framework.Abstractions;
 using Telegram.Bot.Types;
@@ -9,15 +11,25 @@ namespace MyExampleBot.Commands
     {
         public string RawInput { get; set; }
         public string ArgsInput { get; set; }
+
+        public override string ToString()
+        {
+            return new StringBuilder()
+                .Append("RawInput : ").AppendLine(RawInput)
+                .Append("ArgsInput : ").AppendLine(ArgsInput)
+                .ToString();
+        }
     }
     public class HelloCommand : CommandBase<HelloCommandArgs>
     {
-        public HelloCommand() : base("hello")
+        private readonly ILogger<HelloCommand> logger;
+        public HelloCommand(ILogger<HelloCommand> logger) : base("hello")
         {
-            
+            this.logger = logger;
         }
         public override async Task<UpdateHandlingResult> HandleCommand(Update update, HelloCommandArgs args)
         {
+            logger.LogInformation($"Command received {args} with message {update.Message}");
             string replyText = string.IsNullOrWhiteSpace(args.ArgsInput) ? "Hello Who?" : args.ArgsInput;
 
             await Bot.Client.SendTextMessageAsync(
